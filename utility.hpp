0977a8d27d09fb7697e6e510824be746810153a2
Fredi-B <fredibechtoldt@outlook.com>
2023-02-03 16:12:12 +0100

    first commit

---
+++ utility.hpp
@@ -0,0 +1,14 @@
+/* *************** 
+*                 *
+*   ft::utility   *
+*                 *
+ *************** */
+
+#ifndef UTILITY_HPP
+# define UTILITY_HPP
+
+namespace ft { //NAMESPACE ft
+
+}
+
+#endif // UTILITY_HPP
